c87df469795fbf60a0150bc8441fa5f886a5666d
Jim X. Lin <miljinx@gmail.com>
2017-04-30 20:52:11 -0700

    Refactor to allow access to api vars

--- src/App.js
+++ src/App.js
@@ -1,35 +1,69 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as firebase from "firebase";
-import CodeMirror from "codemirror";
+// import { Col, Row } from 'react-bootstrap';
+import * as firebase from 'firebase';
+import CodeMirror from 'codemirror';
+import randomstring from 'randomstring';
 
 class App extends Component{
+  constructor() {
+    super();
+    this.state = {code: 'somestring'};
+    // this.callback = this.callback.bind(this); // this function will detect changes in text
+  }
+
+  componentDidMount() {
+    this.initializeApi();
+  }
+
+  initializeApi() {
+    firebase.initializeApp(config);
+
+    var url = window.location;
+    var token = '';
+    if (url.pathname.length === 1) {
+      token = randomstring.generate();
+      url.pathname = token;
+    } else {
+      token = url.pathname.slice(1, -1);
+    }
+
+    this.codeSession = firebase.database().ref(token)
+    this.codeSession.set('codetext');
+    this.codeMirror = CodeMirror(document.getElementById('codeArea'), { lineWrapping: true });
+  }
+
   render() {
+    // this.codeSession is accessible
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div id='codeArea'></div>
+        <CodeArea codeSession={this.codeSession} />
       </div>
     );
   }
 }
 
-// Initialize Firebase
+class CodeArea extends Component {
+  constructor() {
+    super();
+
+  }
+  render() {
+    return <div id='codeArea'></div>
+  }
+}
+
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOM,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJ_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STOR_BUCKET
 };
-firebase.initializeApp(config);
-
-const firebaseDB = firebase.database().ref();
-
-const codeMirror = CodeMirror(document.getElementById('codeArea'), { lineWrapping: true });
 
 export default App;
