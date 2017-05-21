a3e6f439f7a96dea5bd195a0ba3609aba2db0f1c
Jim X. Lin <miljinx@gmail.com>
2017-05-20 22:40:01 -0700

    Add hackerearth API and code running panel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { Col, Row, Glyphicon } from 'react-bootstrap';
 import * as firebase from 'firebase';
+import hackerEarth from 'hackerearth-node';
 import CodeMirror from 'codemirror';
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/python/python'
-import 'codemirror/mode/ruby/ruby'
 import randomstring from 'randomstring';
 import debounce from 'lodash.debounce';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/ruby/ruby';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -30,7 +31,25 @@ class App extends Component{
     };
   }
 
-  initializeApi() {
+  handleLangChange(event) {
+    var language = event.target.value;
+    this.setState({language: language});
+    this.codeSession.child('language').set(language);
+  }
+
+  handleTabChange(event) {
+    this.setState({tabsize: event.target.value});
+  }
+
+  handleKeyUp(str) {
+    if (this.state.codeString !== str) {
+      this.setState({codeString: str});
+      this.codeSession.child('codeString').set(str);
+      this.codeSession.child('userId').set(this.userId);
+    }
+  }
+
+  componentWillMount() {
     firebase.initializeApp(config);
     var url = window.location;
     var sessionId = '';
@@ -58,28 +77,6 @@ class App extends Component{
     }
   }
 
-  handleLangChange(event) {
-    var language = event.target.value
-    this.setState({language: language});
-    this.codeSession.child('language').set(language);
-  }
-
-  handleTabChange(event) {
-    this.setState({tabsize: event.target.value});
-  }
-
-  handleKeyUp(str) {
-    if (this.state.codeString != str) {
-      this.setState({codeString: str})
-    }
-    this.codeSession.child('codeString').set(str);
-    this.codeSession.child('userId').set(this.userId);
-  }
-
-  componentWillMount() {
-    this.initializeApi();
-  }
-
   componentDidMount() {
     this.codeSession.on('value', snapshot => {
       if (snapshot.val().userId !== this.userId) {
@@ -95,39 +92,41 @@ class App extends Component{
           <div className="logo">RTCE_REPL</div>
           {/* add button to copy url here (use clipboardjs) */}
           <select value={this.state.language} onChange={this.handleLangChange}>
-              <option value="javascript">Javascript</option>
-              <option value="python">Python</option>
-              <option value="ruby">Ruby</option>
+            <option value="JAVASCRIPT">Javascript</option>
+            <option value="PYTHON">Python</option>
+            <option value="RUBY">Ruby</option>
           </select>
           <select value={this.state.tabsize} onChange={this.handleTabChange}>
-            <option value="2">Tabsize: 2</option>
-            <option value="4">Tabsize: 4</option>
-            <option value="8">Tabsize: 8</option>
+            <option value="2">Tab Size: 2</option>
+            <option value="4">Tab Size: 4</option>
+            <option value="8">Tab Size: 8</option>
           </select>
-          <button className="runCode">Run<Glyphicon glyph="play" /></button>
         </div>
         <CodeArea
           codeString={this.state.codeString}
           language={this.state.language}
           tabsize={this.state.tabsize}
-          onKeyUp={this.handleKeyUp}/>
+          onKeyUp={this.handleKeyUp}
+        />
+        <RunResult
+          codeString={this.state.codeString}
+          language={this.state.language}
+        />
       </div>
     );
   }
 }
 
-
-
 class CodeArea extends Component {
   constructor() {
     super();
     this.getCode = this.getCode.bind(this);
-    this.state = {cursor: {line: '0', ch: '0'}} // remember cursor position on re-render
+    this.state = {cursor: {line: '0', ch: '0'}}; // remember cursor position on re-render
   }
 
   getCode() {
-    this.setState({cursor: this.codeMirror.getCursor()})
-    this.props.onKeyUp(this.codeMirror.getValue())
+    this.setState({cursor: this.codeMirror.getCursor()});
+    this.props.onKeyUp(this.codeMirror.getValue());
   }
 
   componentDidMount() {
@@ -138,6 +137,7 @@ class CodeArea extends Component {
       lineNumbers: true,
       indentWithTabs: true
     });
+    this.codeMirror.setSize(null, 500);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -148,7 +148,45 @@ class CodeArea extends Component {
   }
 
   render() {
-    return <div id='codeArea' onKeyUp={debounce(this.getCode, 500)}></div>
+    return <Col md={6} id='codeArea' onKeyUp={debounce(this.getCode, 500)}></Col>
+  }
+}
+
+class RunResult extends Component {
+  constructor() {
+    super();
+    this.handleClickRunCode = this.handleClickRunCode.bind(this);
+  }
+
+  handleClickRunCode() {
+    var config = {
+      time_limit: 2,
+      memory_limit: 131072,
+      source: this.props.codeString,
+      language: this.props.language,
+      async: '1'
+    };
+    this.hackerEarth.run(config).then(result => {
+      console.log(result);
+    }).catch(err => {
+
+    });
+  }
+
+  componentWillMount() {
+    var hackerEarthApiKey = process.env.REACT_APP_HACKEREARTH_API_KEY;
+    this.hackerEarth = new hackerEarth(hackerEarthApiKey);
+  }
+
+  render() {
+    return (
+      <Col md={6} className="runCode-container">
+        <button className="runCode" onClick={this.handleClickRunCode}>
+          Run<Glyphicon glyph="play" />
+        </button>
+        <div id='runResult'>running code result</div>
+      </Col>
+    )
   }
 }
 
